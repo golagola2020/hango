c4ec5e092a3ecd54ecd6763ff1f05ff71c11a54b
parkwoorim <woorimprog@gmail.com>
2020-07-28 22:12:40 +0900

    모바일 통신 테스트
    Signed-off-by: parkwoorim <woorimprog@gmail.com>

--- server/routes/mobile.js
+++ server/routes/mobile.js
@@ -12,7 +12,28 @@ router.post('/login', (req, res) => {
 
 // 회원가입 요청 및 응답
 router.post('/signup', (req, res) => {
-  
+    console.log(req.body);
+    const user = {
+            userName : req.body.userName,
+            userId : req.body.userId,
+            userPasswd : req.body.userPasswd
+        };
+
+    db.query(`INSERT INTO users(name, id, passwd) values('${user.userName}', '${user.userId}', '${user.userPasswd}')`), (err, result) => {
+        // if (err) {
+        //     console.log(err);
+        //     res.json( { success : false } );
+        // }
+        console.log(result);
+        res.json( {success : true} );
+    }
+    // res.json( {success : true} )
+});
+
+router.get('/', (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    res.send("good");
 });
 
 // 모듈 내보내기
