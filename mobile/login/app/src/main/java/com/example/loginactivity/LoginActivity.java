292e800bf2a30a6c08c56daf686e6af4bf15b629
parkwoorim <woorimprog@gmail.com>
2020-07-28 15:26:06 +0900

    로그인 화면 상태바 삭제
    Signed-off-by: parkwoorim <woorimprog@gmail.com>

--- mobile/login/app/src/main/java/com/example/loginactivity/LoginActivity.java
+++ mobile/login/app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -2,12 +2,15 @@ package com.example.loginactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // 상태바 없애기
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
     }
 }
